d86392d600d66c2da3ebc9979478835e4293f870
Felix M. Martinez <f.martinez.espinal@gmail.com>
2022-09-22 23:20:21 -0400

    Adding Vue 3 liting and initial task component

---
+++ src/components/Task-Component.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col-2">
+      {{ props.task.dateKey }}
+    </div>
+    <div class="col-2">
+      {{ props.task.usId }}
+    </div>
+    <div class="col-2">
+      {{ props.task.taskId }}
+    </div>
+    <div class="col-2">
+      {{ props.task.title }}
+    </div>
+    <div class="col-2">
+      {{ props.task.time }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  task: {
+    type: Object,
+    default: () => ({
+      dateKey: "",
+      usId: 0,
+      taskId: 0,
+      title: "",
+      time: 0,
+    }),
+  },
+});
+</script>
--- src/views/Main-View.vue
+++ src/views/Main-View.vue
@@ -1,27 +1,50 @@
 <template>
-  <date-picker @on-date-change="onDateChange($event)" />
-  <div class="col-12">Main window</div>
-  <div>{{ dateKey }}</div>
+  <div>
+    <date-picker @on-date-change="onDateChange($event)" />
+    <div v-for="task in tasks" :key="task.taskId">
+      <task :task="task" />
+    </div>
+  </div>
 </template>
 
 <script>
 import { ref, computed } from "vue";
 import moment from "moment";
 import DatePicker from "@/components/Datepicker-Component.vue";
+import Task from "@/components/Task-Component.vue";
 
 export default {
-  name: "Main-View",
+  name: "MainView",
   components: {
     DatePicker,
+    Task,
   },
   setup() {
     const date = ref(new Date());
     const dateKey = computed(() => moment(date.value).format("YYYYmmDD"));
     const onDateChange = (d) => (date.value = d);
 
+    const tasks = ref([
+      {
+        dateKey: "20220922",
+        usId: 123,
+        taskId: 1123,
+        title: "First Task",
+        time: 3,
+      },
+      {
+        dateKey: "20220923",
+        usId: 456,
+        taskId: 4456,
+        title: "Second Task",
+        time: 5,
+      },
+    ]);
+
     return {
       onDateChange,
       dateKey,
+      tasks,
     };
   },
 };
