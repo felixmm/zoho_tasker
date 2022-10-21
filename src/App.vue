625b41620cdaf80760a6e89e290f4f7c9a1b112e
Felix M. Martinez <f.martinez.espinal@gmail.com>
2022-10-21 08:52:20 -0400

    Removing Vue router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container-fluid h-100">
-    <router-view />
+    <main-view />
   </div>
 </template>
 
 <script>
+import MainView from "@/views/Main.vue";
+
 export default {
   name: "App",
+  components: {
+    MainView,
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
 import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
-import routes from "@/router/routes";
+
 import App from "./App.vue";
 
+// Styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
+// Modal
 import { vfmPlugin } from 'vue-final-modal';
 
 const app = createApp(App);
 
-// Router
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
-app.use(router);
-
+// Plugins
 app.use(vfmPlugin);
 
-// Mount the app
+// Mount
 app.mount("#app");
