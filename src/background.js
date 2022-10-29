a539bde8fd0bc27f51b284bd12e2466f1cac72c0
Felix M. Martinez <f.martinez.espinal@gmail.com>
2022-10-29 07:00:06 -0400

    Removing top menu

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow, Menu } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
 import Store from "electron-store";
@@ -30,6 +30,9 @@ async function createWindow() {
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
     },
   });
+
+  // Removing default menu
+  Menu.setApplicationMenu(null);
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
